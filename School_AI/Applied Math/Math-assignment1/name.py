fca94061bc64b8b841d6591177881cd4dfab3496
ryuchenn <ryuchenforwork@gmail.com>
2024-10-13 15:57:21 -0400

    Delete to AI Folder

--- School_AI/Applied Math/Math-assignment1/name.py
+++
@@ -1,2 +0,0 @@
-def hi_my_name_is():
-    return "Chia Hung Chen" # Write your name inside the quotation marks
--- School_AI/Applied Math/Math-assignment2/numeric_derivation.py
+++
@@ -1,3 +0,0 @@
-
-def derive(f, x, h=0.0001):
-    return (f(x + h) - f(x - h)) / (2 * h)  # TODO: implement this function 
